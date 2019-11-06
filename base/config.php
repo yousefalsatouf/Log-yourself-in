31c9d354a8f10c83594d53079599acda603a05df
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2019-11-06 09:53:07 +0100

    started with the display

--- base/config.php
+++ base/config.php
@@ -2,16 +2,13 @@
 
 function connectionDB()
 {
-    // Try to figure out what these should be for you
     $dbhost = "remotemysql.com";
     $dbName = "pMPjp8OBbe";
     $dbuser = "pMPjp8OBbe";
     $dbpass = "hTShDF49dV";
 
-    // Try to understand what happens here
     $pdo = new PDO("mysql:host=$dbhost;dbname=$dbName", $dbuser, $dbpass);
 
-    // Why we do this here
     return $pdo;
 }
 
